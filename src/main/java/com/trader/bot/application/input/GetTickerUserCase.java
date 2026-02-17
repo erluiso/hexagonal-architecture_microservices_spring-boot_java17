8f3ff84515e2c19bc894acea8a0cbed4ef09fe18
erluiso <yojuanmartinez@gmail.com>
2026-02-17 20:13:42 +0100

    Delete src directory

--- src/main/java/com/trader/bot/application/input/GetTickerUserCase.java
+++
@@ -1,9 +0,0 @@
-package com.trader.bot.application.input;
-
-import com.trader.bot.domain.model.Ticker;
-
-public interface GetTickerUserCase {
-
-    Ticker getTicker(String symbol);
-
-}
--- src/main/java/com/trader/bot/application/output/TickerOutputPort.java
+++
@@ -1,9 +0,0 @@
-package com.trader.bot.application.output;
-
-import com.trader.bot.domain.model.Ticker;
-
-public interface TickerOutputPort {
-
-    Ticker getTicker(String symbol);
-
-}
